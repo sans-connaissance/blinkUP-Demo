b69c790ea0943b7f2966705fd8cd884ffc001933
David Malicke <davemalicke@gmail.com>
2022-08-09 20:26:01 -0400

    Initial Commit

---
+++ blinkUP-Demo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  blinkUP-Demo
+//
+//  Created by David Malicke on 8/9/22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ blinkUP-Demo/blinkUP_DemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  blinkUP_DemoApp.swift
+//  blinkUP-Demo
+//
+//  Created by David Malicke on 8/9/22.
+//
+
+import SwiftUI
+
+@main
+struct blinkUP_DemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
