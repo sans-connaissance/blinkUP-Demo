394f54754d0a6bb17ef3944677bea35b04139354
David Malicke <davemalicke@gmail.com>
2022-08-09 21:45:41 -0400

    Adding mock data models

---
+++ blinkUP-Demo/Models/Group.swift
@@ -0,0 +1,36 @@
+//
+//  Group.swift
+//  blinkUP-Demo
+//
+//  Created by David Malicke on 8/9/22.
+//
+
+import Foundation
+
+struct Group: Codable, Identifiable, Hashable {
+    var id: Int
+    var name: String?
+    var shortDescription: String?
+    var longDescription: String?
+    var message: [Message]?
+    var members: [Friend]?
+    var meetUps: [MeetUp]?
+}
+
+struct Message: Codable, Identifiable, Hashable {
+    var id: Int
+    var message: String?
+    var likes: Int?
+    var postedBy: Friend?
+    
+}
+
+struct MeetUp: Codable, Identifiable, Hashable {
+    var id: Int
+    var name: String?
+    var numberOfAttendees: Int?
+    
+}
+
+
+
---
+++ blinkUP-Demo/Networking/MockDataStore.swift
@@ -0,0 +1,41 @@
+//
+//  MockDataStore.swift
+//  blinkUP-Demo
+//
+//  Created by David Malicke on 8/9/22.
+//
+
+import Foundation
+
+let bLinkupTeam = Group(
+    id: 1,
+    name: "bLinkup Team",
+    shortDescription: "Members of the bLinkup production team.",
+    longDescription: "Members of the bLinkup production team. In ligula diam, cursus lobortis sollicitudin ut, vulputate at nunc. Cras posuere tortor at commodo laoreet. Ut vel faucibus enim. Donec tincidunt viverra tortor, a consequat orci condimentum sed. Suspendisse lacinia mollis tincidunt. Donec efficitur neque orci, nec ultrices mauris pharetra vel.",
+    message: nil,
+    members: nil,
+    meetUps: nil)
+
+
+let sanDiegoStateAztecs = Group(
+    id: 2,
+    name: "San Diego State Aztecs Fans",
+    shortDescription: "Fans of the SDSU Aztecs.",
+    longDescription: "In ligula diam, cursus lobortis sollicitudin ut, vulputate at nunc. Cras posuere tortor at commodo laoreet. Ut vel faucibus enim. Donec tincidunt viverra tortor, a consequat orci condimentum sed. Suspendisse lacinia mollis tincidunt. Donec efficitur neque orci, nec ultrices mauris pharetra vel.",
+    message: nil,
+    members: nil,
+    meetUps: nil)
+
+
+let alabamaCrimsonTide = Group(
+    id: 2,
+    name: "Crimson Tide Football fans",
+    shortDescription: "Fans of the Crimpson Tide.",
+    longDescription: "In ligula diam, cursus lobortis sollicitudin ut, vulputate at nunc. Cras posuere tortor at commodo laoreet. Ut vel faucibus enim. Donec tincidunt viverra tortor, a consequat orci condimentum sed. Suspendisse lacinia mollis tincidunt. Donec efficitur neque orci, nec ultrices mauris pharetra vel.",
+    message: nil,
+    members: nil,
+    meetUps: nil)
+
+
+
+
